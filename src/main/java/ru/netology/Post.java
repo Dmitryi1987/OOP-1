44a410b670a8381edbd68071811fd783ac971d67
Dmitry1987 <vasyutinskii5@gmail.com>
2023-04-03 20:23:33 +0300

    First commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+public class Post {
+
+    public FormDate birthday;
+    public String name; // поле для имени
+
+    public String passport; // поле для паспорта
+
+    public String patronymic; // поле для отчества
+
+    public String phone; // поле для номера телефона
+
+    public String surname; // поле для фамилии
+
+    public boolean subscriptions; // поле согласия получать рассылку по sms
+
+}
